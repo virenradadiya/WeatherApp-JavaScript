25f240b545e99d3d1f9e3f256c3f92d3cc21763a
Viren Radadiya <virenradadiya7@gmail.com>
2020-08-25 17:52:28 +0530

    WeatherApp-JavaScript

---
+++ app.js
@@ -0,0 +1,87 @@
+//API keys: f997006fade1ddfa8ade83141b982416
+//select element
+const iconElement = document.querySelector(".weather-icon");
+const tempElement = document.querySelector(".temperature-value p ");
+const descElement = document.querySelector(".temperature-description p");
+const locationElement = document.querySelector(".location p");
+const notificationElement = document.querySelector(".notification");
+
+//App data
+const weather ={};
+weather.temperature = {
+    unit : "Celsius"
+};
+const KELVIN = 273;
+const key = "f997006fade1ddfa8ade83141b982416";
+
+//check broweser location
+if('geolocation' in navigator){
+    navigator.geolocation.getCurrentPosition(setPosition, showError);
+}
+else{
+    notificationElement.style.display = "block";
+    notificationElement.innerHTML ="<p>Browser Does not Support Location</p>";
+}
+
+//set user position
+function setPosition(position){
+    let latitude = position.coords.latitude;
+    let longitude = position.coords.longitude;
+
+    getWeather(latitude, longitude);
+}
+//show error where there is any issue with location service
+function showError(error){
+    notificationElement.style.display = "block";
+    notificationElement.innerHTML =`<p>${ error.message }</p>`;
+}
+
+//get weather from api provoder
+function getWeather(latitude, longitude){
+    let api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&exclude={part}&appid=${key}`
+
+    fetch(api)
+        .then(function(response){
+            let data = response.json();
+            return data;
+        })
+        .then(function(data){
+            weather.temperature.value = Math.floor(data.main.temp - KELVIN);
+            weather.description = data.weather[0].description;
+            weather.iconId = data.weather[0].icon;
+            weather.city = data.name;
+            weather.country = data.sys.country;
+        })
+        .then(function(){
+            displayWeather();
+        });
+}
+
+// Disolay weather to UI
+function displayWeather(){
+    iconElement.innerHTML = `<img src="icons/${weather.iconId}.png"/>`;
+    tempElement.innerHTML = `${weather.temperature.value}°<span>C</span>`;
+    descElement.innerHTML = weather.description;
+    locationElement.innerHTML = `${weather.city}, ${weather.country}`;
+}
+
+// C to F conversion
+function celsiusToFahrenheit(temperature){
+    return (temperature * 9/5) + 32;
+}
+
+// WHEN THE USER CLICKS ON THE TEMPERATURE ELEMENET
+tempElement.addEventListener("click", function(){
+    if(weather.temperature.value === undefined) return;
+    
+    if(weather.temperature.unit == "celsius"){
+        let fahrenheit = celsiusToFahrenheit(weather.temperature.value);
+        fahrenheit = Math.floor(fahrenheit);
+        
+        tempElement.innerHTML = `${fahrenheit}°<span>F</span>`;
+        weather.temperature.unit = "fahrenheit";
+    }else{
+        tempElement.innerHTML = `${weather.temperature.value}°<span>C</span>`;
+        weather.temperature.unit = "celsius"
+    }
+});
